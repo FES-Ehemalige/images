3c300f1ed6ab9a1cc010ca847c58a5a32ad32f02
kaktus42 <vowinkel.alexander@gmail.com>
2024-11-10 11:01:38 +0100

    change "Abgabedatum" to "Ende / Wiedervorlage"

--- redmine/setup.sh
+++ redmine/setup.sh
@@ -17,3 +17,5 @@ export RAILS_ENV="$REDMINE_ENV"
 run_as_user redmine bundle config set frozen false
 run_as_user redmine bundle install
 run_as_user redmine bundle config unset frozen
+
+sed -i 's#field_due_date: Abgabedatum#field_due_date: Ende / Wiedervorlage#' config/locales/de.yml
