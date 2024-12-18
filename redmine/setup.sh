22769720b3e9a9328523943755008752e97ab066
Alexander Vowinkel <https://github.com/kaktus42>
2024-12-18 17:58:53 +0100

    fix breaking build because of missing libraries

--- redmine/setup.sh
+++ redmine/setup.sh
@@ -3,7 +3,7 @@
 set -e
 
 apt-get update
-apt-get install -y subversion ssh build-essential zlib1g-dev libpq-dev default-libmysqlclient-dev
+apt-get install -y subversion ssh curl wget build-essential zlib1g-dev libpq-dev default-libmysqlclient-dev
 
 mkdir -p /home/redmine/.ssh/
 
@@ -22,3 +22,6 @@ run_as_user redmine bundle config unset frozen
 
 # customize ticket field naming
 sed -i 's#field_due_date: Abgabedatum#field_due_date: Ende / Wiedervorlage#' config/locales/de.yml
+
+sed -i 's#redmine_rake_execute "db:migrate" >/dev/null 2>&1#redmine_rake_execute "db:migrate"#' /opt/bitnami/scripts/libredmine.sh
+sed -i 's#redmine_rake_execute "redmine:plugins:migrate" >/dev/null 2>&1#redmine_rake_execute "redmine:plugins:migrate"#' /opt/bitnami/scripts/libredmine.sh
